c5f2b8d487f3d99a7bf3991b6700752eba4e234a
Bridget <bridget.njoki@student.moringaschool.com>
2024-08-19 19:07:59 +0300

    add

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,15 @@
 // Your code here
+export function pointsForWord(word) {
+  let points = 0;
+  for (const char of word.toLowerCase()) {  // Convert to lowercase to handle case sensitivity
+    if (/[aeiou]/.test(char)) {
+      points += 1;
+    } else if (/[a-z]/.test(char)) {  // Ensure we only count letters
+      points += 2;
+    }
+  }
+  return points;
+}
+
+
+
